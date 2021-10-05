93b4cead136358db8df00811942714bf54bb0721
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-10-05 15:30:32 +0600

    updates

--- app/src/main/java/org/tensorflow/lite/examples/digitclassifier/DigitClassifier.kt
+++ app/src/main/java/org/tensorflow/lite/examples/digitclassifier/DigitClassifier.kt
@@ -12,129 +12,130 @@ import android.graphics.Bitmap
 import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks.call
+import org.tensorflow.lite.Interpreter
+import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import org.tensorflow.lite.Interpreter
-import java.io.File
 
 class DigitClassifier(private val context: Context) {
-  private var interpreter: Interpreter? = null
-  var isInitialized = false
-    private set
+    private var interpreter: Interpreter? = null
+    var isInitialized = false
+        private set
 
-  /** Executor to run inference task in the background */
-  private val executorService: ExecutorService = Executors.newCachedThreadPool()
+    /** Executor to run inference task in the background */
+    private val executorService: ExecutorService = Executors.newCachedThreadPool()
 
-  private var inputImageWidth: Int = 0 // will be inferred from TF Lite model
-  private var inputImageHeight: Int = 0 // will be inferred from TF Lite model
-  private var modelInputSize: Int = 0 // will be inferred from TF Lite model
+    private var inputImageWidth: Int = 0 // will be inferred from TF Lite model
+    private var inputImageHeight: Int = 0 // will be inferred from TF Lite model
+    private var modelInputSize: Int = 0 // will be inferred from TF Lite model
 
-  fun initialize(model: Any): Task<Void> {
-    return call(
-      executorService,
-      Callable<Void> {
-        initializeInterpreter(model)
-        null
-      }
-    )
-  }
-
-  private fun initializeInterpreter(model: Any) {
-    // Initialize TF Lite Interpreter with NNAPI enabled
-    val options = Interpreter.Options()
-    options.setUseNNAPI(true)
-    var interpreter: Interpreter
-    if (model is ByteBuffer) {
-      interpreter = Interpreter(model, options)
-    } else {
-      interpreter = Interpreter(model as File, options)
-    }
-    // Read input shape from model file
-    val inputShape = interpreter.getInputTensor(0).shape()
-    inputImageWidth = inputShape[1]
-    inputImageHeight = inputShape[2]
-    modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth * inputImageHeight * PIXEL_SIZE
-
-    // Finish interpreter initialization
-    this.interpreter = interpreter
-    isInitialized = true
-    Log.d(TAG, "Initialized TFLite interpreter.")
-  }
-
-  private fun classify(bitmap: Bitmap): String {
-    if (!isInitialized) {
-      throw IllegalStateException("TF Lite Interpreter is not initialized yet.")
+    fun initialize(model: Any): Task<Void> {
+        return call(
+            executorService,
+            Callable<Void> {
+                initializeInterpreter(model)
+                null
+            }
+        )
     }
 
-    var startTime: Long
-    var elapsedTime: Long
+    fun initializeInterpreter(model: Any) {
+        // Initialize TF Lite Interpreter with NNAPI enabled
+        val options = Interpreter.Options()
+        options.setUseNNAPI(true)
+        var interpreter: Interpreter
+        if (model is ByteBuffer) {
+            interpreter = Interpreter(model, options)
+        } else {
+            interpreter = Interpreter(model as File, options)
+        }
+        // Read input shape from model file
+        val inputShape = interpreter.getInputTensor(0).shape()
+        inputImageWidth = inputShape[1]
+        inputImageHeight = inputShape[2]
+        modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth * inputImageHeight * PIXEL_SIZE
 
-    // Preprocessing: resize the input
-    startTime = System.nanoTime()
-    val resizedImage = Bitmap.createScaledBitmap(bitmap, inputImageWidth, inputImageHeight, true)
-    val byteBuffer = convertBitmapToByteBuffer(resizedImage)
-    elapsedTime = (System.nanoTime() - startTime) / 1000000
-    Log.d(TAG, "Preprocessing time = " + elapsedTime + "ms")
-
-    startTime = System.nanoTime()
-    val result = Array(1) { FloatArray(OUTPUT_CLASSES_COUNT) }
-    interpreter?.run(byteBuffer, result)
-    elapsedTime = (System.nanoTime() - startTime) / 1000000
-    Log.d(TAG, "Inference time = " + elapsedTime + "ms")
-
-    return getOutputString(result[0])
-  }
-
-  fun classifyAsync(bitmap: Bitmap): Task<String> {
-    return call(executorService, Callable<String> { classify(bitmap) })
-  }
-
-  fun close() {
-    call(
-      executorService,
-      Callable<String> {
-        interpreter?.close()
-        Log.d(TAG, "Closed TFLite interpreter.")
-        null
-      }
-    )
-  }
-
-  private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
-    val byteBuffer = ByteBuffer.allocateDirect(modelInputSize)
-    byteBuffer.order(ByteOrder.nativeOrder())
-
-    val pixels = IntArray(inputImageWidth * inputImageHeight)
-    bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
-
-    for (pixelValue in pixels) {
-      val r = (pixelValue shr 16 and 0xFF)
-      val g = (pixelValue shr 8 and 0xFF)
-      val b = (pixelValue and 0xFF)
-
-      // Convert RGB to grayscale and normalize pixel value to [0..1]
-      val normalizedPixelValue = (r + g + b) / 3.0f / 255.0f
-      byteBuffer.putFloat(normalizedPixelValue)
+        // Finish interpreter initialization
+        this.interpreter = interpreter
+        isInitialized = true
+        Log.d(TAG, "Initialized TFLite interpreter.")
     }
 
-    return byteBuffer
-  }
+    private fun classify(bitmap: Bitmap): String {
+        if (!isInitialized) {
+            throw IllegalStateException("TF Lite Interpreter is not initialized yet.")
+        }
 
-  private fun getOutputString(output: FloatArray): String {
-    val maxIndex = output.indices.maxBy { output[it] } ?: -1
-    return "Prediction Result: %d\nConfidence: %2f".format(maxIndex, output[maxIndex])
-  }
+        var startTime: Long
+        var elapsedTime: Long
 
-  companion object {
-    private const val TAG = "DigitClassifier"
+        // Preprocessing: resize the input
+        startTime = System.nanoTime()
+        val resizedImage =
+            Bitmap.createScaledBitmap(bitmap, inputImageWidth, inputImageHeight, true)
+        val byteBuffer = convertBitmapToByteBuffer(resizedImage)
+        elapsedTime = (System.nanoTime() - startTime) / 1000000
+        Log.d(TAG, "Preprocessing time = " + elapsedTime + "ms")
 
-    private const val FLOAT_TYPE_SIZE = 4
-    private const val PIXEL_SIZE = 1
+        startTime = System.nanoTime()
+        val result = Array(1) { FloatArray(OUTPUT_CLASSES_COUNT) }
+        interpreter?.run(byteBuffer, result)
+        elapsedTime = (System.nanoTime() - startTime) / 1000000
+        Log.d(TAG, "Inference time = " + elapsedTime + "ms")
 
-    private const val OUTPUT_CLASSES_COUNT = 10
-    private const val MODEL_NAME_KEY = "model_name"
-  }
+        return getOutputString(result[0])
+    }
+
+    fun classifyAsync(bitmap: Bitmap): Task<String> {
+        return call(executorService, Callable<String> { classify(bitmap) })
+    }
+
+    fun close() {
+        call(
+            executorService,
+            Callable<String> {
+                interpreter?.close()
+                Log.d(TAG, "Closed TFLite interpreter.")
+                null
+            }
+        )
+    }
+
+    private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val byteBuffer = ByteBuffer.allocateDirect(modelInputSize)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val pixels = IntArray(inputImageWidth * inputImageHeight)
+        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+        for (pixelValue in pixels) {
+            val r = (pixelValue shr 16 and 0xFF)
+            val g = (pixelValue shr 8 and 0xFF)
+            val b = (pixelValue and 0xFF)
+
+            // Convert RGB to grayscale and normalize pixel value to [0..1]
+            val normalizedPixelValue = (r + g + b) / 3.0f / 255.0f
+            byteBuffer.putFloat(normalizedPixelValue)
+        }
+
+        return byteBuffer
+    }
+
+    private fun getOutputString(output: FloatArray): String {
+        val maxIndex = output.indices.maxByOrNull { output[it] } ?: -1
+        return "Prediction Result: %d\nConfidence: %2f".format(maxIndex, output[maxIndex])
+    }
+
+    companion object {
+        private const val TAG = "DigitClassifier"
+
+        private const val FLOAT_TYPE_SIZE = 4
+        private const val PIXEL_SIZE = 1
+
+        private const val OUTPUT_CLASSES_COUNT = 10
+        private const val MODEL_NAME_KEY = "model_name"
+    }
 }
